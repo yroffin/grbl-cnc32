94536d21bff2363e0a2da26ab3ec17ddf33ca3f3
Yannick Roffin <yroffin@gmail.com>
2020-07-27 00:12:40 +0200

    Update

---
+++ ui/src/app/app.main.ts
@@ -0,0 +1,62 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.main';
+import { AppComponent } from './app.component';
+import { ConsoleComponent } from './components/console/console.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { CheckboxModule } from 'primeng/checkbox';
+import { TabViewModule } from 'primeng/tabview';
+import { ButtonModule } from 'primeng/button';
+import { PaperjsComponent } from './components/paperjs/paperjs.component';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { ToastModule } from 'primeng/toast';
+import { SliderModule } from 'primeng/slider';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreService, ActionListenerEffects } from './store/store.service';
+import { TerminalModule } from 'primeng/terminal';
+import { BabylonComponent } from './components/babylon/babylon.component';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConsoleComponent,
+    PaperjsComponent,
+    BabylonComponent
+  ],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot(StoreService.reducers),
+    EffectsModule.forRoot([ActionListenerEffects]),
+    AppRoutingModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    InputSwitchModule,
+    CheckboxModule,
+    TabViewModule,
+    ButtonModule,
+    RadioButtonModule,
+    ToastModule,
+    SliderModule,
+    TerminalModule,
+    MessageModule,
+    MessagesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
